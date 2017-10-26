460b5fcbcb7b594d55e81fd2afad8fc5383a32b2
Wouter Demuynck <wouter.am.demuynck@gmail.com>
2017-10-27 01:26:54 +0200

    Further improved styling of command results.

--- src/components/Console/Command.js
+++ src/components/Console/Command.js
@@ -2,18 +2,31 @@ import React from 'react'
 import { FONT_SIZE, GRID, PRIMARY_TEXT, SECONDARY_TEXT, DOMINANT_COLOR } from '../theme'
 
 const style = {
+    color: PRIMARY_TEXT,
+    fontSize: FONT_SIZE,
+    padding: `${ GRID * .75 }px 0`,
+    display: 'flex',
+    justifyContent: 'space-between',
+}
+const commandStyle = {
     color: DOMINANT_COLOR,
     fontSize: FONT_SIZE * 0.85,
     padding: `${ GRID * .75 }px 0`,
     fontStyle: 'bold',
 }
+const messageStyle = {
+    paddingLeft: GRID * 4,
+    color: DOMINANT_COLOR,
+    fontSize: FONT_SIZE * 0.85,
+}
 
 const Command = (props) => {
-    const { message } = props
+    const { statement, message } = props
 
     return (
         <div style={ style }>
-            { message }
+            <span style={ commandStyle }>{ statement }</span>
+            <span style={ messageStyle }>{ message }</span>
         </div>
     )
 
