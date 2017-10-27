fcc32727c65428c11097ffc65ec586bcf408a0a6
Wouter Demuynck <wouter.am.demuynck@gmail.com>
2017-10-27 15:27:46 +0200

    Working on the Tokenizer.

--- src/api/tokenizer.js
+++ src/api/tokenizer.js
@@ -10,30 +10,32 @@ export default class Tokenizer {
     moveNext() {
         this._lastToken = null;
 
-        let currentKind = TokenKind.IDENTIFIER;
+        let currentKind = TokenKind.UNDEFINED;
         let currentValue = '';
 
         while (this.position < this.expression.length) {
             let current = this.expression[this.position];
 
             if (/[A-Za-z0-9]/g.test(current)) {
+                if (currentKind !== TokenKind.IDENTIFIER && currentKind !== TokenKind.UNDEFINED) {
+                    break;
+                }
                 currentValue += current;
                 currentKind = TokenKind.IDENTIFIER;
             } else if (/[-\+]|[0-9]|[\.]/g.test(currentValue)) {
+                if (currentKind !== TokenKind.LITERAL && currentKind !== TokenKind.UNDEFINED) {
+                    break;
+                }
                 currentValue += current;
                 currentKind = TokenKind.LITERAL;
             } else if (/\=|\/|\*/g.test(current)) {
+                if (currentKind !== TokenKind.OPERATOR && currentKind !== TokenKind.UNDEFINED) {
+                    break;
+                }
                 currentValue += current;
                 currentKind = TokenKind.OPERATOR;
             } else if (current == ' ') {
-                this._lastToken = {
-                    position: (this.position - currentValue.length),
-                    value: currentValue,
-                    kind: currentKind
-                }
-                
-                this.position++;
-                return true;
+
             }
             
             this.position++;
